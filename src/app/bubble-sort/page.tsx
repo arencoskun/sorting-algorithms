d02ec3e8d760c4481ce9a4924e2685f9bc94b7a6
arencoskun <ar3n1596@gmail.com>
2024-01-31 14:00:15 +0300

    Add stats to bubble sort page

--- src/app/bubble-sort/page.tsx
+++ src/app/bubble-sort/page.tsx
@@ -6,7 +6,6 @@ import { bubbleSortStep, fillDataArray, sleep } from "../utils";
 import Button from "@/components/Button";
 import NumberPicker from "@/components/NumberPicker";
 import { useRouter } from "next/navigation";
-import Head from "next/head";
 
 export default function Home() {
   const [data, setData] = useState<number[]>(fillDataArray(10));
@@ -14,16 +13,25 @@ export default function Home() {
   const [searchIndex, setSearchIndex] = useState<number>(0);
   const [arrLength, setArrLength] = useState<number>(0);
   const [started, setStarted] = useState<boolean>(false);
+  const [iterCount, setIterCount] = useState<number>(0);
+  const [arrayAccessCount, setArrayAccessCount] = useState<number>(0);
+  const [swapCount, setSwapCount] = useState<number>(0);
   const router = useRouter();
 
   const setDataArr = () => {
     setData(fillDataArray(arrLength ? arrLength : 10));
     setSortIndex(0);
+    setIterCount(0);
+    setArrayAccessCount(0);
+    setSwapCount(0);
   };
 
   const step = async () => {
     let tempData: number[] = data;
     let swapped: boolean = false;
+    let tempIterCount: number = 0;
+    let tempAccessCount: number = 0;
+    let tempSwapCount: number = 0;
     setStarted(true);
     for (let i = 0; i < tempData.length; i++) {
       swapped = false;
@@ -31,19 +39,25 @@ export default function Home() {
         var stepResult = bubbleSortStep(tempData, j);
         tempData = stepResult[0] as number[];
         swapped = stepResult[1] as boolean;
-        //console.log(swapped);
         setSearchIndex(j);
         setSortIndex(i);
         setData(tempData);
+        setIterCount(tempIterCount);
+        setArrayAccessCount(tempAccessCount);
+        setSwapCount(tempSwapCount);
         await sleep(100);
+        tempIterCount++;
+        tempAccessCount += 7;
+        if (swapped) tempSwapCount++;
       }
+      tempIterCount++;
       if (swapped === false && sortIndex > 1) break;
     }
     setStarted(false);
   };
 
   return (
-    <main className="flex h-screen flex-col items-center justify-center p-24 py-24 space-y-4">
+    <main className="flex h-screen flex-col items-center justify-center p-24 py-32 space-y-4">
       <Typography>Bubble sort</Typography>
       <div className="flex flex-row justify-center">
         <NumberPicker
@@ -62,6 +76,10 @@ export default function Home() {
           Back
         </Button>
       </div>
+      <Typography>
+        {iterCount} iterations, {arrayAccessCount} array accesses, {swapCount}{" "}
+        swaps
+      </Typography>
       <VictoryBar
         name="bar"
         height={250}
